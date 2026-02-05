d908d5e343e656ea02adc998d84bf09b5b868ed5
Solomind <solomind@Solo.local>
2026-02-05 15:05:15 +0800

    Fix: Use correct dbPath

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 const app = express();
 const PORT = parseInt(process.env.PORT) || 3001;
-const dbPath = '/tmp/geothermal.db';
+const dbPath = path.join(__dirname, 'data/geothermal.db');
 
 let db = null;
 
